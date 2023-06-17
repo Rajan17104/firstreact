8efc91f6d15be9756387f1290fb78d89059be501
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-06-17 07:48:14 +0530

    17-6

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import CounterFun from './container/CounterFun';
 import Counter1 from './container/Counter1';
 import Country from './container/Country';
 import CountryFun from './container/CountryFun';
+import Product from './container/Product';
 
 function App() {
   return (
     <>
     {/* <Counter1 /> */}
-    <Employee />
+    {/* <Employee /> */}
     {/* <EmployeeFun /> */}
     {/* <CounterFun /> */}
     {/* <Country /> */}
     {/* <CountryFun /> */}
+    <Product />
     </>
   );
 }
